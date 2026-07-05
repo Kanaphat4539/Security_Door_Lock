c5ebbea4a4241c26ebeaeb5fe914e0d212ae0729
Kanaphat Rungrapeepornpong <changchana2123@gmail.com>
2026-07-05 14:16:47 +0700

    Scaffold complete module structure

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { AccessLogsModule } from './modules/access-logs/access-logs.module';
+import { HardwareModule } from './modules/hardware/hardware.module';
+import { AppGateway } from './gateways/app/app.gateway';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AccessLogsModule, HardwareModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppGateway],
 })
 export class AppModule {}
